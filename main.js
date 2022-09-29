61ed1c78ac08063ad894745e1ad90462ff20b1d2
Juan Sebastian Echeverry Parada <jsebast32@gmail.com>
2022-09-29 12:18:47 -0500

    include aside container with tmy order content (clase13.html) within index.html

--- main.js
+++ main.js
@@ -2,20 +2,16 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const hambMenu = document.querySelector('.menu');       // Hamburguer menu in mobile version
 const mobileMenu = document.querySelector('.mobile-menu');
+const cartIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 // Iteration with account menu in desktop version
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
-    /*
-    // My first solution
-    if (desktopMenu.classList.contains('hidden')) {
-        desktopMenu.classList.remove('hidden');
-    } else {
-        desktopMenu.classList.add('hidden');
-    }
-    */
+    hideElement(aside);
+    
     // JuanDC solution
     desktopMenu.classList.toggle('hidden');
 }
@@ -25,5 +21,27 @@ function toggleDesktopMenu() {
 hambMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu() {
+    hideElement(aside);
     mobileMenu.classList.toggle('hidden');
+}
+
+// Iteration with shopping cart icon
+
+cartIcon.addEventListener('click', toggleAsideContent);
+
+function toggleAsideContent() {
+    hideElement(desktopMenu);
+    hideElement(mobileMenu);
+
+    aside.classList.toggle('hidden');
+}
+
+// Function to apply the class selector hidden to hide the element
+// @element: selector required to be hidden
+function hideElement(element) {
+    const isHiddenElement = element.classList.contains('hidden');
+
+    if (!isHiddenElement) {
+        element.classList.add('hidden');
+    }
 }
