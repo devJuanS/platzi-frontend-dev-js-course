197d7209a1e94c5d327a414c29e3d58193993ff2
Juan Sebastian Echeverry Parada <jsebast32@gmail.com>
2022-09-29 10:50:58 -0500

    include mobile menu (clase8.html) within index.html

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hambMenu = document.querySelector('.menu');       // Hamburguer menu in mobile version
+const mobileMenu = document.querySelector('.mobile-menu');
+
+// Iteration with account menu in desktop version
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
+    /*
+    // My first solution
     if (desktopMenu.classList.contains('hidden')) {
         desktopMenu.classList.remove('hidden');
     } else {
         desktopMenu.classList.add('hidden');
     }
+    */
+    // JuanDC solution
+    desktopMenu.classList.toggle('hidden');
+}
 
-    /*desktopMenu.classList.toggle('hidden');*/
+// Iteration with menu in mobile version
+
+hambMenu.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('hidden');
 }
